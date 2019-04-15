a32aca2e3ac0b6ee6729b8c71f3911e7057082c8
JC Kim <jckdotim@gmail.com>
2019-04-15 09:20:53 +0700

    Fix __init__ bug

--- flask_gfm.py
+++ flask_gfm.py
@@ -4,5 +4,5 @@ from mdx_gfm import GithubFlavoredMarkdownExtension
 
 class Markdown(BaseMarkdown):
     def __init__(self, app, auto_escape=False, **markdown_options):
-        super(self).__init__(app, auto_escape, markdown_options)
-        self.register_extension(GithubFlavoredMarkdownExtension())
+        super().__init__(app=app, auto_escape=auto_escape)
+        self.register_extension(GithubFlavoredMarkdownExtension)
