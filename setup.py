fa83aa09a77b9057247863c73585922b9b4637dc
JCKim <jckdotim@gmail.com>
2019-04-17 14:15:25 +0900

    Change initial version from 0.0.0 to 0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-GFM',
-    version='0.0.0',
+    version='0.1.0',
     url='https://github.com/jckdotim/flask-gfm/',
     license='MIT',
     author='JC Kim',
