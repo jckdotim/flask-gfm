56b0c14df213fe947a9fe0d09d69a496ad1cf8a9
JC Kim <jckdotim@gmail.com>
2019-04-27 23:20:11 +0900

    v1.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-GFM',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/jckdotim/flask-gfm/',
     license='MIT',
     author='JC Kim',
